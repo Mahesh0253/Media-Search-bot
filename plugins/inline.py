83f38d151195cad671b868400f682162d0a5cb6f
Mahesh0253 <maheshmalekar0253@gmail.com>
2020-09-17 15:55:54 +0530

    Fixed empty caption

--- plugins/inline.py
+++ plugins/inline.py
@@ -30,7 +30,7 @@ async def answer(bot, query):
             InlineQueryResultCachedDocument(
                 title=file.file_name,
                 file_id=file.file_id,
-                caption=file.caption,
+                caption=file.caption or "",
                 description=f'Size: {get_size(file.file_size)}\nType: {file.file_type}',
                 reply_markup=reply_markup))
 
