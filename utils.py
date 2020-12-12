e9e6da4d9ab7a8b2b3d08c9b216d059d1534867a
Mahesh0253 <maheshmalekar0253@gmail.com>
2020-12-12 21:16:17 +0530

    improved search results

--- utils.py
+++ utils.py
@@ -30,7 +30,7 @@ class Media(Document):
 
 async def save_file(media):
     """Save file in database"""
-    
+
     try:
         file = Media(
             file_id=media.file_id,
@@ -58,7 +58,7 @@ async def save_file(media):
 async def get_search_results(query, file_type=None, max_results=10, offset=0):
     """For given query return (results, next_offset)"""
 
-    raw_pattern = query.lower().strip().replace(' ', '.?')
+    raw_pattern = query.lower().strip().replace(' ', '.*')
     if not raw_pattern:
         raw_pattern = '.'
 
