3bb8567aa79d8277188d330cfb1a3a1bdcc2dbca
Mahesh0253 <maheshmalekar0253@gmail.com>
2020-12-13 13:12:10 +0530

    Fixed AUTH_USERS filter

--- plugins/inline.py
+++ plugins/inline.py
@@ -1,11 +1,11 @@
 from urllib.parse import quote
-from pyrogram import Client, emoji
+from pyrogram import Client, emoji, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument
 from utils import get_search_results
-from info import MAX_RESULTS, CACHE_TIME, SHARE_BUTTON_TEXT
+from info import MAX_RESULTS, CACHE_TIME, SHARE_BUTTON_TEXT, AUTH_USERS
 
 
-@Client.on_inline_query()
+@Client.on_inline_query(filters.user(AUTH_USERS) if AUTH_USERS else None)
 async def answer(bot, query):
     """Show search results for given inline query"""
 
