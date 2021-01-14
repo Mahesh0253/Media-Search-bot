414266ae6ce58283b105a6104001c14f54ba6b45
Mahesh0253 <maheshmalekar0253@gmail.com>
2021-01-14 15:39:01 +0530

    small fix

--- info.py
+++ info.py
@@ -16,7 +16,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 # Admins, Channels & Users
 ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in environ['ADMINS'].split()]
 CHANNELS = [int(ch) if re.search('^.\d+$', ch) else ch for ch in environ['CHANNELS'].split()]
-auth_users = [int(user) if re.search('^\d+$', user) else user for user in environ['AUTH_USERS'].split()]
+auth_users = [int(user) if re.search('^\d+$', user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
 # MongoDB information
