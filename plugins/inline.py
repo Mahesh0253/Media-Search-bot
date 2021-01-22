8e6f6cc3530ef130c68ab94a6bd62a15283c8e26
rgrrocks <renjithgr18@gmail.com>
2021-01-22 20:05:20 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -58,11 +58,7 @@ async def answer(bot, query):
 
 def get_reply_markup(username):
     url = 't.me/share/url?url=' + quote(SHARE_BUTTON_TEXT.format(username=username))
-    buttons = [[
-        InlineKeyboardButton('Search again', switch_inline_query_current_chat=''),
-        InlineKeyboardButton('Share bot', url=url),
-    ]]
-    return InlineKeyboardMarkup(buttons)
+      return InlineKeyboardMarkup(buttons)
 
 
 def get_size(size):
