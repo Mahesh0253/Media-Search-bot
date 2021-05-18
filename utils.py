4e8f396e5829ae07ec3dc526118b66e5addf1525
Mahesh <44301650+Mahesh0253@users.noreply.github.com>
2021-05-18 21:47:13 +0530

    Fix regex pattern

--- utils.py
+++ utils.py
@@ -70,9 +70,9 @@ async def get_search_results(query, file_type=None, max_results=10, offset=0):
     if not query:
         raw_pattern = '.'
     elif ' ' not in query:
-        raw_pattern = f'[\b\.\+\-_]{query}[b\.\+\-_]'
+        raw_pattern =r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
     else:
-        raw_pattern = query.replace(' ', '[\s\.\+\-_]')
+        raw_pattern = query.replace(' ', r'[\s\.\+\-_]')
 
     try:
         regex = re.compile(raw_pattern, flags=re.IGNORECASE)
