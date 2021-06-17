48af1a2879e6794479092881b4ba447be4f188ad
Mahesh0253 <maheshmalekar0253@gmail.com>
2021-06-17 11:25:25 +0530

    Fix caption

--- utils.py
+++ utils.py
@@ -47,7 +47,7 @@ async def save_file(media):
             file_size=media.file_size,
             file_type=media.file_type,
             mime_type=media.mime_type,
-            caption=media.caption.html if media.caption else None,
+            caption=media.caption.html if media.caption else '',
         )
     except ValidationError:
         logger.exception('Error occurred while saving file in database')
