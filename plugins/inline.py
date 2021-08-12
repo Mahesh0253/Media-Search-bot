032b4c143fbc30bf3037926ddf4cb69f9428afb3
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2021-08-12 09:40:39 +0530

    Add Error Message 😆

--- plugins/inline.py
+++ plugins/inline.py
@@ -61,12 +61,19 @@ async def answer(bot, query):
         if string:
             switch_pm_text += f" for {string}"
 
-        await query.answer(results=results,
+        try:
+            await query.answer(results=results,
                            is_personal = True,
                            cache_time=cache_time,
                            switch_pm_text=switch_pm_text,
                            switch_pm_parameter="start",
                            next_offset=str(next_offset))
+        except Exception as e:
+            logging.exception(str(e))
+            await query.answer(results=[], is_personal=True,
+                           cache_time=cache_time,
+                           switch_pm_text=str(e)[:63],
+                           switch_pm_parameter="error")
     else:
 
         switch_pm_text = f'{emoji.CROSS_MARK} No results'
