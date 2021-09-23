fdd067e8656de40a9d4d8fa903acd1b84fa0b1b7
Mahesh <44301650+Mahesh0253@users.noreply.github.com>
2021-09-23 12:11:32 +0530

    Improve search pattern

--- utils.py
+++ utils.py
@@ -69,7 +69,7 @@ async def get_search_results(query, file_type=None, max_results=10, offset=0):
     elif ' ' not in query:
         raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
     else:
-        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_]')
+        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_\(\)]')
 
     try:
         regex = re.compile(raw_pattern, flags=re.IGNORECASE)
