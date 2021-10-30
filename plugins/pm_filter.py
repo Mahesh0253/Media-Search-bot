20342730ac41e2f59e64138d7447d8e51cc932be
Beast <91755865+thehellruler@users.noreply.github.com>
2021-10-30 08:24:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -367,6 +367,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 await query.message.reply_to_message.delete()
                 await query.message.delete()
             except:
-                pass
+                await query.message.delete()
+                pas
     else:
         await query.answer("കൌതുകും ലേശം കൂടുതൽ ആണല്ലേ👀",show_alert=True)
