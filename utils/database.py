0e9287d91c1fcb92464f611469322154f174fb4b
Mahesh <44301650+Mahesh0253@users.noreply.github.com>
2022-01-13 22:17:31 +0530

    Fix error handling

--- utils/database.py
+++ utils/database.py
@@ -71,7 +71,7 @@ async def get_search_results(query, file_type=None, max_results=10, offset=0):
     try:
         regex = re.compile(raw_pattern, flags=re.IGNORECASE)
     except:
-        return []
+        return [], ''
 
     if USE_CAPTION_FILTER:
         filter = {'$or': [{'file_name': regex}, {'caption': regex}]}
