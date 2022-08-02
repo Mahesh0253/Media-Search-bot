a82149717c048e825ba798511447e436e7f581a3
Renjith Mangal <65238086+m4mallu@users.noreply.github.com>
2022-08-02 21:34:57 +0530

    Update bot.py
    Module level import standardized !

--- bot.py
+++ bot.py
@@ -1,14 +1,12 @@
-import logging
 import logging.config
-
-# Get logging configurations
-logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.ERROR)
-
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from utils import Media
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN
+
+# Get logging configurations
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.ERROR)
 
 
 class Bot(Client):
