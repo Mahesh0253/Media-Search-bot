1abcbb2c8ce234f85d1d14a847e6baba81a33956
Thaju <93075984+Thajudecodes@users.noreply.github.com>
2023-02-03 23:34:51 +0530

    Update userbot.py
    I have cleared some of the main errors chek it out

--- plugins/userbot.py
+++ plugins/userbot.py
@@ -12,46 +12,51 @@ lock = asyncio.Lock()
 
 
 @Client.on_message(filters.command(['index', 'indexfiles']) & filters.user(ADMINS))
-async def index_files(bot, message):
+async def index_files(client, message):
     """Save channel or group files with the help of user bot"""
 
     if not USERBOT_STRING_SESSION:
         await message.reply('Set `USERBOT_STRING_SESSION` in info.py file or in environment variables.')
-    elif len(message.command) == 1:
+        return
+
+    if len(message.command) == 1:
         await message.reply('Please specify channel username or id in command.\n\nExample: `/index -10012345678`')
-    elif lock.locked():
+        return
+
+    if lock.locked():
         await message.reply('Wait until previous process complete.')
-    else:
-        msg = await message.reply('Processing...⏳')
-        raw_data = message.command[1:]
-        user_bot = Client('User-bot', API_ID, API_HASH, session_string=USERBOT_STRING_SESSION)
-        chats = [int(chat) if id_pattern.search(chat) else chat for chat in raw_data]
-        total_files = 0
+        return
 
-        async with lock:
-            try:
-                async with user_bot:
-                    for chat in chats:
+    msg = await message.reply('Processing...⏳')
 
-                        async for user_message in user_bot.get_chat_history(chat):
-                            try:
-                                message = await bot.get_messages(chat, user_message.id, replies=0)
-                            except FloodWait as e:
-                                await asyncio.sleep(e.value)
-                                message = await bot.get_messages(chat, user_message.id, replies=0)
+    raw_data = message.command[1:]
+    user_bot = Client('User-bot', API_ID, API_HASH, session_string=USERBOT_STRING_SESSION)
+    chats = [int(chat) if id_pattern.search(chat) else chat for chat in raw_data]
+    total_files = 0
 
-                            for file_type in ("document", "video", "audio"):
-                                media = getattr(message, file_type, None)
-                                if media is not None:
-                                    break
-                            else:
-                                continue
-                            media.file_type = file_type
-                            media.caption = message.caption
-                            await save_file(media)
-                            total_files += 1
-            except Exception as e:
-                logger.exception(e)
-                await msg.edit(f'Error: {e}')
-            else:
-                await msg.edit(f'Total {total_files} checked!')
+    async with lock:
+        try:
+            async with user_bot:
+                for chat in chats:
+                    async for user_message in user_bot.get_chat_history(chat):
+                        try:
+                            message = await client.get_messages(chat, user_message.id, replies=0)
+                        except FloodWait as e:
+                            await asyncio.sleep(e.value)
+                            message = await client.get_messages(chat, user_message.id, replies=0)
+
+                        for file_type in ("document", "video", "audio"):
+                            media = getattr(message, file_type, None)
+                            if media is not None:
+                                break
+                        else:
+                            continue
+                        media.file_type = file_type
+                        media.caption = message.caption
+                        await save_file(media)
+                        total_files += 1
+        except Exception as e:
+            logger.exception(e)
+            await msg.edit(f'Error: {e}')
+        else:
+            await msg.edit(f'Total {total_files} checked!')
